046bfc9db72dc630b85b8c3d4c65f49266fb0c44
Dev Mello <caiuabrandao@gmail.com>
2024-05-25 16:56:06 -0300

    changes done 2

--- src/Componentes/Navbar.js
+++ src/Componentes/Navbar.js
@@ -16,19 +16,19 @@ export default function Navbar({ isLoggedIn }) {
                 <div className="nav-items">
                     <div className="nav-item">
                         <Link to='/cadastrar' className="nav-link sem-icons">Novo Hóspede</Link>
-                        <Link to='/cadastrar' className="nav-link icons"><img alt="hospede-icon" src={icons.hospede} /></Link>
+                        <Link to='/cadastrar' className="nav-link icons"><img alt="hospede-icon" src={icons.Addhospede} /></Link>
                     </div>
                     <div className="nav-item">
                         <Link to='/cadastros' className="nav-link sem-icons">Hóspedes</Link>
-                        <Link to='/cadastros' className="nav-link icons"><img alt="Addhospede-icon" src={icons.Addhospede} /></Link>
+                        <Link to='/cadastros' className="nav-link icons"><img alt="Addhospede-icon" src={icons.hospede} /></Link>
                     </div>
                     <div className="nav-item">
                         <Link to='/reservar' className="nav-link sem-icons">Nova Reserva</Link>
-                        <Link to='/reservar' className="nav-link icons"><img alt="addReserva" src={icons.Reservas} /></Link>
+                        <Link to='/reservar' className="nav-link icons"><img alt="addReserva" src={icons.addReserva} /></Link>
                     </div>
                     <div className="nav-item">
                         <Link to='/reservas' className="nav-link sem-icons">Ver Reservas</Link>
-                        <Link to='/reservas' className="nav-link icons"><img alt="Reservas" src={icons.addReserva} /></Link>
+                        <Link to='/reservas' className="nav-link icons"><img alt="Reservas" src={icons.Reservas} /></Link>
                     </div>
                 </div>
             </div>
