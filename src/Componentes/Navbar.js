7ab5bcf88b7cecd3a937daef027395af2124ab78
Dev Mello <caiuabrandao@gmail.com>
2024-05-25 16:54:49 -0300

    changes done

--- src/Componentes/Navbar.js
+++ src/Componentes/Navbar.js
@@ -24,11 +24,11 @@ export default function Navbar({ isLoggedIn }) {
                     </div>
                     <div className="nav-item">
                         <Link to='/reservar' className="nav-link sem-icons">Nova Reserva</Link>
-                        <Link to='/reservar' className="nav-link icons"><img alt="addReserva" src={icons.addReserva} /></Link>
+                        <Link to='/reservar' className="nav-link icons"><img alt="addReserva" src={icons.Reservas} /></Link>
                     </div>
                     <div className="nav-item">
                         <Link to='/reservas' className="nav-link sem-icons">Ver Reservas</Link>
-                        <Link to='/reservas' className="nav-link icons"><img alt="Reservas" src={icons.Reservas} /></Link>
+                        <Link to='/reservas' className="nav-link icons"><img alt="Reservas" src={icons.addReserva} /></Link>
                     </div>
                 </div>
             </div>
