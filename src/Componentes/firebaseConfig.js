991c97a58a73e9e64ba6a7f66beaf1da67fdb2a2
Dev Mello <caiuabrandao@gmail.com>
2024-06-02 01:27:16 -0300

    Firebase Project Changed

--- src/Componentes/firebaseConfig.js
+++ src/Componentes/firebaseConfig.js
@@ -1,13 +1,13 @@
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBQV_foANHA8iO-mEnThoqJ70Ozi1M65bI",
-    authDomain: "pousada-ca6ee.firebaseapp.com",
-    projectId: "pousada-ca6ee",
-    storageBucket: "pousada-ca6ee.appspot.com",
-    messagingSenderId: "802819734988",
-    appId: "1:802819734988:web:8dfbfc702c862f15363169",
-    measurementId: "G-EMHK5HFJBZ"
+    apiKey: "AIzaSyAb1hcGf-nSOa7HVZiWOtIGKA4i9OZV6IM",
+    authDomain: "pousada-970ed.firebaseapp.com",
+    projectId: "pousada-970ed",
+    storageBucket: "pousada-970ed.appspot.com",
+    messagingSenderId: "889802809993",
+    appId: "1:889802809993:web:3eab544e01e261e76c1c58",
+    measurementId: "G-NXTEV0R92E"
 };
 
 
